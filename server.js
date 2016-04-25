4502d9cbcb8829147a472b891c5bb305c094f7eb
Andrei Scutariu <Andrei.Scutariu@endava.com>
2016-04-25 19:52:37 +0300

    build

--- server.js
+++ server.js
@@ -1,21 +1,23 @@
-var webpack = require('webpack')
-var webpackDevMiddleware = require('webpack-dev-middleware')
-var webpackHotMiddleware = require('webpack-hot-middleware')
-var config = require('./webpack.config')
+if (process.env.NODE_ENV !== 'production') {
+  var webpack = require('webpack')
+  var webpackDevMiddleware = require('webpack-dev-middleware')
+  var webpackHotMiddleware = require('webpack-hot-middleware')
+  var config = require('./webpack.config')
 
-var app = new (require('express'))()
-var port = process.env.PORT || 8089
+  var app = new (require('express'))()
+  var port = process.env.PORT || 8089
 
 
-var compiler = webpack(config)
-app.use(webpackDevMiddleware(compiler, { noInfo: true, publicPath: config.output.publicPath }))
-app.use(webpackHotMiddleware(compiler))
+  var compiler = webpack(config)
+  app.use(webpackDevMiddleware(compiler, { noInfo: true, publicPath: config.output.publicPath }))
+  app.use(webpackHotMiddleware(compiler))
+}
 
-app.get("/", function(req, res) {
+app.get("/", function (req, res) {
   res.sendFile(__dirname + '/index.html')
 })
 
-app.listen(port, function(error) {
+app.listen(port, function (error) {
   if (error) {
     console.error(error)
   } else {
