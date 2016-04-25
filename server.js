0bb8e2dac20a1ffcd696958be04200ee5198e158
AndreiScutariu <andreiscutariu92@gmail.com>
2016-04-25 21:35:49 +0300

    all files

--- server.js
+++ server.js
@@ -1,21 +1,17 @@
+var port = process.env.PORT || 8089
+var express = require("express")
+var app = express()
+
 if (process.env.NODE_ENV !== 'production') {
   var webpack = require('webpack')
   var webpackDevMiddleware = require('webpack-dev-middleware')
   var webpackHotMiddleware = require('webpack-hot-middleware')
   var config = require('./webpack.config')
-
-  var app = new (require('express'))()
-  var port = process.env.PORT || 8089
-
-
+  
   var compiler = webpack(config)
   app.use(webpackDevMiddleware(compiler, { noInfo: true, publicPath: config.output.publicPath }))
   app.use(webpackHotMiddleware(compiler))
 }
-
-var express = require("express")
-
-app = express();
 
 app.use(express.static(__dirname + "/dist"))
 
